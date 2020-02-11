a9a4c52182d127a8eca8522bb5bd9d5392aa25a7
Sergei Alimov <sergmadox@yandex.ru>
2020-02-11 16:25:49 +0300

    add threading add queue add checking exists path

--- parser_hltv.py
+++ parser_hltv.py
@@ -1,11 +1,83 @@
 import requests
 from bs4 import BeautifulSoup
+import os
+import threading
+from queue import Queue
+from os import path
+import csv
+
+#Класс потока
+class Downloader(threading.Thread):
+    """Потоковый обработчик профилей"""
+
+    def __init__(self, queue):
+        """Инициализация потока"""
+        threading.Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        """Запуск потока"""
+        while True:
+            # Получаем url из очереди
+            url = self.queue.get()
+
+            # Скачиваем файл
+            self.checking_info(url)
+
+            # Отправляем сигнал о том, что задача завершена
+            self.queue.task_done()
+
+
+    def checking_info(self, url):
+        """Смотрим профиль игрока и пишем в файл"""
+        data = []
+        import re
+        pattern = re.compile(r"(\w+\W)$|(\w+)$")
+        name_player = pattern.search(url)
+        data.append(['Player', name_player[0]])
+        info_page = get_html(url)
+        print (info_page)
+        soup = BeautifulSoup(info_page, "html.parser")
+        table = soup.find('div', attrs={'class': 'statistics'}).find_all('div', class_='stats-row')
+
+        for row in table:
+            cols = row.find_all('span')
+            data.append([x.text for x in cols])
+
+        with open('player_ind_stat.csv', 'a') as f:
+            writer = csv.writer(f, delimiter=';')
+            res = []
+            for i in range(len(data)):
+                res.append(data[i][1])
+            writer.writerow(res)
+
+
+def main(urls):
+    """
+    Запускаем программу
+    """
+    queue = Queue()
+
+    # Запускаем потом и очередь
+    for i in range(5):
+        t = Downloader(queue)
+        t.setDaemon(True)
+        t.start()
+
+    # Даем очереди нужные нам ссылки для скачивания
+    for url in urls:
+        concat_url = 'https://www.hltv.org'+str(url)
+        queue.put(concat_url)
+
+    # Ждем завершения работы очереди
+    queue.join()
 
 #Делаем запрос на таблицу игрков
 def get_html(url):
-    r = requests.get(url)
-    r.encoding = 'utf-8'
-    return r.text
+    r = requests.Session()
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0'}
+    res = r.get(url, headers=headers)
+    return res.text
 
 #Получаем список игроков и персональный url на их страницу со статистикой
 def get_info(url):
@@ -22,37 +94,32 @@ def get_info(url):
 
     return data
 
-#Переходим по персональным url игроков и тащим статистику
-def info_player(url):
-    data = []
-    import re
-    pattern = re.compile(r"(\w+)$")
-    name_player = pattern.search(url)
-    data.append(['Player',name_player[0]])
-
-    info_page = get_html(url)
-
-    soup = BeautifulSoup(info_page, "html.parser")
-    table = soup.find('div', attrs={'class': 'statistics'}).find_all('div', class_='stats-row')
-
-    for row in table:
-        cols = row.find_all('span')
-        data.append([x.text for x in cols])
-
-    return data
-
-def write_csv(data):
-    import csv
+def write_csv():
+    columns = ['Player',
+            'Total kills',
+            'Headshot %',
+            'Total deaths',
+            'K/D Ratio',
+            'Damage / Round',
+            'Grenade dmg / Round',
+            'Maps played',
+            'Rounds played',
+            'Kills / round',
+            'Assists / round',
+            'Deaths / round',
+            'Saved by teammate / round',
+            'Saved teammates / round',
+            'Rating 1.0',
+            ]
     with open('player_ind_stat.csv', 'a') as f:
-        writer = csv.writer(f)
-        writer.writerow((data[0],data[1]))
+        writer = csv.DictWriter(f, delimiter=';', fieldnames=columns)
+        writer.writeheader()
 
+if path.exists('player_ind_stat.csv') == False:
+    write_csv()
 
 stat_page = get_html('https://www.hltv.org/stats/players/')
 d = get_info(stat_page)
-srting_page_player = 'https://www.hltv.org' + str(d[1])
+main(d)
 
 
-result = info_player(srting_page_player)
-print (result)
-write_csv(result)
